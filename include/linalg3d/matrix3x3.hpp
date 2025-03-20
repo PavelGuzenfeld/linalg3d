aff8639f7f3ba831aba55c7ce41a080c3da37d3c
Pavel Guzenfeld <me@pavelguzenfeld.com>
2025-03-20 20:26:27 +0200

    Add matrix multiplication operator and inversion correctness test for Matrix3

--- include/linalg3d/matrix3x3.hpp
+++ include/linalg3d/matrix3x3.hpp
@@ -95,6 +95,22 @@ namespace linalg3d
                 m[2][0] * scalar, m[2][1] * scalar, m[2][2] * scalar};
         }
 
+        [[nodiscard]] constexpr Matrix3 operator*(const Matrix3 &other) const noexcept
+        {
+            return Matrix3{
+                m[0][0] * other.m[0][0] + m[0][1] * other.m[1][0] + m[0][2] * other.m[2][0],
+                m[0][0] * other.m[0][1] + m[0][1] * other.m[1][1] + m[0][2] * other.m[2][1],
+                m[0][0] * other.m[0][2] + m[0][1] * other.m[1][2] + m[0][2] * other.m[2][2],
+
+                m[1][0] * other.m[0][0] + m[1][1] * other.m[1][0] + m[1][2] * other.m[2][0],
+                m[1][0] * other.m[0][1] + m[1][1] * other.m[1][1] + m[1][2] * other.m[2][1],
+                m[1][0] * other.m[0][2] + m[1][1] * other.m[1][2] + m[1][2] * other.m[2][2],
+
+                m[2][0] * other.m[0][0] + m[2][1] * other.m[1][0] + m[2][2] * other.m[2][0],
+                m[2][0] * other.m[0][1] + m[2][1] * other.m[1][1] + m[2][2] * other.m[2][1],
+                m[2][0] * other.m[0][2] + m[2][1] * other.m[1][2] + m[2][2] * other.m[2][2]};
+        }
+
         [[nodiscard]] constexpr Matrix3 operator/(double scalar) const noexcept
         {
             return Matrix3{
